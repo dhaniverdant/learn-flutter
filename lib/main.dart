f719b16e69b6f25931cf5b30efeff950881c3f92
dhani <dhaniverdant23@gmail.com>
2020-01-02 18:59:06 +0700

    change main view

--- lib/main.dart
+++ lib/main.dart
@@ -4,17 +4,26 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
-  var container = Container(
-    // grey box
-    child: Text(
-      "Lorem ipsum",
-      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24.0, fontFamily: 'Roboto'),
+  var container = Container( // grey box
+    child: Center(
+      child: Container( // red box
+        child: Text(
+          "Lorem ipsum",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 24.0,
+          ),
+        ),
+        decoration: BoxDecoration(
+          color: Colors.red[400],
+        ),
+        padding: EdgeInsets.all(16),
+        width: 240, //max-width is 240
+      ),
     ),
     width: 320,
     height: 240,
-    decoration: BoxDecoration(
-      color: Colors.yellow[300],
-    ),
+    color: Colors.grey[300],
   );
   Widget build(BuildContext context) {
     return MaterialApp(
